a1ee751c425c3bd1e63c803a62c37e41ffd85683
Carlo Nery de Lima Moro <carlo.moro@wises.com.br>
2024-01-30 15:14:41 -0300

    Update version and dependencies in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
+import platform
 
-setup(
-    name='minivectordb',
-    version='1.1.4',
-    author='Carlo Moro',
-    author_email='cnmoro@gmail.com',
-    description="This is a Python project aimed at extracting embeddings from textual data and performing semantic search.",
-    packages=find_packages(),
-    package_data={
-        'minivectordb': ['resources/embedding_model_quantized.onnx']
-    },
-    include_package_data=True,
-    install_requires=[
+architecture = platform.machine().lower()
+
+is_arm = 'arm' in architecture or 'aarch64' in architecture
+
+if is_arm:
+    reqs = [
+        "numpy",
+        "transformers",
+        "faiss-cpu",
+        "torch",
+        "pytest",
+        "pytest-cov",
+        "rank-bm25",
+        "thefuzz[speedup]"
+    ]
+else:
+    reqs = [
         "numpy",
         "onnx",
         "onnxruntime",
@@ -23,7 +29,20 @@ setup(
         "pytest-cov",
         "rank-bm25",
         "thefuzz[speedup]"
-    ],
+    ]
+
+setup(
+    name='minivectordb',
+    version='1.1.9',
+    author='Carlo Moro',
+    author_email='cnmoro@gmail.com',
+    description="This is a Python project aimed at extracting embeddings from textual data and performing semantic search.",
+    packages=find_packages(),
+    package_data={
+        'minivectordb': ['resources/embedding_model_quantized.onnx']
+    },
+    include_package_data=True,
+    install_requires=reqs,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
